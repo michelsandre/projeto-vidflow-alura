81eba92c72fbfd7c8ff9e7c6d471f7d9024a6238
Andre Michels de Oliveira <michelsandre@gmail.com>
2023-11-08 12:57:38 -0300

    inclue func barra de pesquisa

--- script.js
+++ script.js
@@ -10,21 +10,48 @@ json-sever --watch backend/videos.json
 
 const containerVideos = document.querySelector(".videos__container");
 
-const api = fetch("http://localhost:3000/videos")
-  .then((res) => res.json())
-  .then((videos) =>
+async function buscarEMostrarVideos() {
+  try {
+    const busca = await fetch("http://localhost:3000/videos");
+    const videos = await busca.json();
+
     videos.forEach((video) => {
+      if (video.descricao == "") {
+        throw new Error("Video sem descrição");
+      }
       containerVideos.innerHTML += `
-    <li class="videos__item">
-        <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
-        <div class="descricao-video">
-            <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
-            <h3 class="titulo-video">${video.titulo}</h3>
-            <p class="titulo-canal">${video.descricao}</p>
-        </div>
-    </li>`;
-    })
-  )
-  .catch((error) => {
+        <li class="videos__item">
+            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+            <div class="descricao-video">
+                <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
+                <h3 class="titulo-video">${video.titulo}</h3>
+                <p class="titulo-canal">${video.descricao}</p>
+            </div>
+        </li>`;
+    });
+  } catch (error) {
     containerVideos.innerHTML = `<p>Erro ao carregar os vídeos: ${error}</p>`;
+  }
+}
+
+buscarEMostrarVideos();
+
+const barraPesquisa = document.querySelector(".pesquisar__input");
+barraPesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa() {
+  const videos = document.querySelectorAll(".videos__item");
+  const valorFiltro = barraPesquisa.value.toLowerCase();
+
+  videos.forEach((video) => {
+    const titulo = video
+      .querySelector(".titulo-video")
+      .textContent.toLowerCase();
+
+    video.style.display = valorFiltro
+      ? titulo.includes(valorFiltro)
+        ? "block"
+        : "none"
+      : "block";
   });
+}
